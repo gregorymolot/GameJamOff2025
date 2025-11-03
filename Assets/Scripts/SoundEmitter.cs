cc14b0fc3f58f224fbb8bae1899e31c397faeb90
gregorymolot <molo0037@algonquinlive.com>
2025-11-02 20:45:35 -0500

    Did some sound emitter stuff

--- Assets/Scripts/SoundEmitter.cs
+++ Assets/Scripts/SoundEmitter.cs
@@ -1,10 +1,28 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
 public class SoundEmitter : MonoBehaviour
 {
     SphereCollider sphere;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    [SerializeField]
+    public bool constant;
+
+    [NonSerialized]
+    public float startingSize;
+
+    [NonSerialized]
+    public float maxSize;
+
+    [NonSerialized]
+    public float timeToReachMaxSize;
+
+    [NonSerialized]
+    public float timeAtMaxSize;
+
     void Start()
     {
         sphere = GetComponent<SphereCollider>();
@@ -27,5 +45,39 @@ public class SoundEmitter : MonoBehaviour
     {
         if (sphere != null)
             Gizmos.DrawSphere(transform.position, sphere.radius);
+    }
+}
+
+[CustomEditor(typeof(SoundEmitter))]
+public class EmitterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SoundEmitter emitter = (SoundEmitter)target;
+
+        DrawDefaultInspector();
+
+        if (emitter.constant)
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Constant Size Options", EditorStyles.boldLabel);
+
+            emitter.startingSize = EditorGUILayout.FloatField("Constant Start Size", emitter.startingSize);
+        }
+
+        else
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Growing Size Options", EditorStyles.boldLabel);
+
+            emitter.maxSize = EditorGUILayout.FloatField("Growing Max Size", emitter.maxSize);
+            emitter.timeToReachMaxSize = EditorGUILayout.FloatField("Time To Reach Max", emitter.timeToReachMaxSize);
+            emitter.timeAtMaxSize = EditorGUILayout.FloatField("Time At Max", emitter.timeAtMaxSize);
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(emitter);
+        }
     }
 }
